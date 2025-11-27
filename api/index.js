6e4248812c91298a2d59730d00fb3317213fab70
Bernardo-Vidal <bernardorosa.bg015@academico.ifsul.edu.br>
2025-11-27 08:24:58 -0300

    integração vercell

--- api/index.js
+++ api/index.js
@@ -1,24 +1,34 @@
-import express from 'express';
-import { fileURLToPath } from 'url';
-import { dirname, join } from 'path';
-import routes from '../routes/route.js'; // rotas externas
-import { createServer } from 'http';
+import { createServer } from 'http'
+
+import express from "express";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+import routes from "../routes/route.js"; // rotas externas
+import pacienteRoutes from "../routes/PacienteRoutes.js";
+import laboratorioRoutes from "../routes/LaboratorioRoutes.js";
+import loteRoutes from "../routes/LoteRoutes.js";
+import vacinaRoutes from "../routes/VacinaRoutes.js";
+import profissionalRoutes from "../routes/ProfissionalRoutes.js";
 
 const app = express();
-
 app.use(express.urlencoded({ extended: true }));
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // Caminho correto das views e public
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 // Servir arquivos estáticos
-app.use(express.static(join(__dirname, '../public')));
-app.set('views', join(__dirname, '../views'));
+app.use(express.static(join(__dirname, "../public")));
+app.set("views", join(__dirname, "../views"));
 
 // Rotas
-app.use(routes)
-app.listen(3001)
+app.use(pacienteRoutes);
+app.use(laboratorioRoutes);
+app.use(loteRoutes);
+app.use(vacinaRoutes);
+app.use(profissionalRoutes);
+app.use(routes);
+app.listen(3000);
 // Exporta o handler compatível com Vercel
 export default app;
